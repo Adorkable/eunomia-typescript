0af0927a9de89160639b4e3792d5993de0715eef
Ian G <yo.ian.g@gmail.com>
2023-07-10 12:52:53 -0400

    match output from jest-junit with reporter expectation

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  reporters: ["default", "jest-junit"],
+  reporters: ["default", ["jest-junit", { outputName: "jest-junit.xml" }]],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/__tests__/__testUtilities__.ts'
