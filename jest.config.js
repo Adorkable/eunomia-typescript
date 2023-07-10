464d9a8a0124853fe1b59dc6c49dd72e3323c71a
Ian G <yo.ian.g@gmail.com>
2023-07-10 12:46:31 -0400

    add reporter jest-junit

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  reporters: ["default", "jest-junit"],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/__tests__/__testUtilities__.ts'
